d9bd96c295b739023ecbc98b09de87acb94f3afe
Osagie-Godstand <51753700+Osagie-Godstand@users.noreply.github.com>
2023-06-02 23:58:38 +0100

    Added a new feature to the types.Product file.

--- types/product.go
+++ types/product.go
@@ -7,16 +7,20 @@ import (
 
 const minProductNameLen = 3
 
+const minProductPriceLen = 1
+
 type Product struct {
-	ID   string `bson:"_id,omitempty" json:"id"`
-	SKU  string `bson:"sku" json:"sku"`
-	Name string `bson:"name" json:"name"`
-	Slug string `bson:"slug" json:"slug"`
+	ID    string  `bson:"_id,omitempty" json:"id"`
+	SKU   string  `bson:"sku" json:"sku"`
+	Name  string  `bson:"name" json:"name"`
+	Slug  string  `bson:"slug" json:"slug"`
+	Price float64 `bson:"price" json:"price"`
 }
 
 type CreateProductRequest struct {
-	SKU  string `json:"sku"`
-	Name string `json:"name"`
+	SKU   string  `json:"sku"`
+	Name  string  `json:"name"`
+	Price float64 `json:"price"`
 }
 
 func NewProductFromRequest(req *CreateProductRequest) (*Product, error) {
@@ -28,9 +32,10 @@ func NewProductFromRequest(req *CreateProductRequest) (*Product, error) {
 	slug := strings.Join(parts, "-")
 
 	return &Product{
-		SKU:  req.SKU,
-		Name: req.Name,
-		Slug: slug,
+		SKU:   req.SKU,
+		Name:  req.Name,
+		Slug:  slug,
+		Price: req.Price,
 	}, nil
 }
 
@@ -40,6 +45,9 @@ func validateCreateProductRequest(req *CreateProductRequest) error {
 	}
 	if len(req.Name) < minProductNameLen {
 		return fmt.Errorf("the name of the product is to short")
+	}
+	if (req.Price) < 1 {
+		return fmt.Errorf("the Price of the product is too low")
 	}
 	return nil
 }
